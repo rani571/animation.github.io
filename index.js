280ed547a06b819a80a17631dc429d779d687c87
rani571 <ranikumari4356867@gmail.com>
2022-08-04 23:21:56 +0530

    animation

---
+++ index.js
@@ -0,0 +1,9 @@
+//here 'Typed' is a class name so we have to write it with capital letters and 'typed' is a object name.
+var typed = new Typed('#mytext' , {
+    strings : ['Javascript Animations', 'Rani kumari'] ,
+    typeSpeed : 50
+})
+
+AOS.init({
+    duration : 2000
+})
